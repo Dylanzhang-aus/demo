e8ae808359e9d0a20043aa2f1ce75b8a81df7cd8
HANYUAN ZHANG <88271768+Dylanzhang-aus@users.noreply.github.com>
2021-09-04 22:03:23 +1000

    Update config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const oktaAuthConfig = {
-    issuer: 'https://dev-44012506.okta.com/oauth2/default',
-    clientId: '0oa1nfbaiy18rcJTt5d7',
+    issuer: 'https://*******/oauth2/default',
+    clientId: '*****',
     redirectUri: window.location.origin + '/login/callback',
   };
   
   const oktaSignInConfig = {
-    baseUrl: 'https://dev-44012506.okta.com',
-    clientId: '0oa1nfbaiy18rcJTt5d7',
+    baseUrl: 'https://******',
+    clientId: '******',
     redirectUri: window.location.origin + '/login/callback',
     authParams: {
 
